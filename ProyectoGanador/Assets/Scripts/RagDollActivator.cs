d48be22a33bdc4ec8fa9be7e8d55af7086c8fc8b
El Bts <angelo06@ucm.es>
2022-02-12 17:29:43 +0100

    RagDoll en mona china sentada pero levantada no

---
+++ ProyectoGanador/Assets/Scripts/RagDollActivator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagDollActivator : MonoBehaviour
+{
+    Animator anim;
+    SphereCollider col;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.gameObject.GetComponent<Animator>();
+
+        col = this.gameObject.GetComponent<SphereCollider>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.GetComponent<BatHit>() != null)
+        {
+            anim.enabled = false;
+
+            col.enabled = false;
+        }
+    }
+}
